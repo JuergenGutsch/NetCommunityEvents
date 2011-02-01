8752b3504c3a2e6fa4ed2d33f615ddfffce961b8
JuergenGutsch <juergen@gutsch-online.de>
2011-02-01 07:21:01 +0100

    Initial Commit

---
+++ NetCommunityEvents.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetCommunityEvents.Controllers;
+
+namespace NetCommunityEvents.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
